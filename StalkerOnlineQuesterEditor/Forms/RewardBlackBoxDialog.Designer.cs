cbfe6f7aa32ff32f6044b1e9e03a985e67337fc7
Wolfgar <wolfgar29@gmail.com>
2018-10-30 16:52:55 +0500

    .

---
+++ StalkerOnlineQuesterEditor/Forms/RewardBlackBoxDialog.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace StalkerOnlineQuesterEditor.Forms
+{
+    partial class RewardBlackBoxDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.itemGridView = new System.Windows.Forms.DataGridView();
+            this.bCancel = new System.Windows.Forms.Button();
+            this.bOk = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.itemGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // itemGridView
+            // 
+            this.itemGridView.AllowUserToOrderColumns = true;
+            this.itemGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.itemGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name});
+            this.itemGridView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.itemGridView.Location = new System.Drawing.Point(0, 0);
+            this.itemGridView.Name = "itemGridView";
+            this.itemGridView.Size = new System.Drawing.Size(275, 258);
+            this.itemGridView.TabIndex = 1;
+            // 
+            // bCancel
+            // 
+            this.bCancel.Location = new System.Drawing.Point(182, 264);
+            this.bCancel.Name = "bCancel";
+            this.bCancel.Size = new System.Drawing.Size(75, 23);
+            this.bCancel.TabIndex = 3;
+            this.bCancel.Text = "Отмена";
+            this.bCancel.UseVisualStyleBackColor = true;
+            // 
+            // bOk
+            // 
+            this.bOk.Location = new System.Drawing.Point(101, 264);
+            this.bOk.Name = "bOk";
+            this.bOk.Size = new System.Drawing.Size(75, 23);
+            this.bOk.TabIndex = 2;
+            this.bOk.Text = "Ок";
+            this.bOk.UseVisualStyleBackColor = true;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.Width = 150;
+            // 
+            // RewardBlackBoxDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 296);
+            this.Controls.Add(this.bCancel);
+            this.Controls.Add(this.bOk);
+            this.Controls.Add(this.itemGridView);
+            this.Name = "RewardBlackBoxDialog";
+            this.Text = "RewardBlackBoxDialog";
+            ((System.ComponentModel.ISupportInitialize)(this.itemGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView itemGridView;
+        private System.Windows.Forms.DataGridViewComboBoxColumn name;
+        private System.Windows.Forms.Button bCancel;
+        private System.Windows.Forms.Button bOk;
+    }
+}
