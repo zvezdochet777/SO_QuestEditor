ace792759c37da023ad179e2ec71ba6cabca005c
Wolfgar <wolfgar29@gmail.com>
2022-04-04 17:00:08 +0500

    .

---
+++ StalkerOnlineQuesterEditor/ResourceClasses/CAchivmentsConstants.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StalkerOnlineQuesterEditor
+{
+    public static class CAchivements
+    {
+        static string PATH = "../../../res/scripts/common/achivments/achivements.pyson";
+        static string LOCAL_PATH = CSettings.pathToLocalFiles + CSettings.ORIGINAL_PATH + "/Achivements.xml";
+        static Dictionary<int, string> achivements = new Dictionary<int, string>();
+
+        public static void load()
+        {
+            if (!File.Exists(PATH))
+                return;
+
+            string line;
+            StreamReader reader = new StreamReader(PATH);
+            int id = 0;
+            string name = "";
+
+
+
+            while ((line = reader.ReadLine()) != null)
+            {
+
+                if (!line.Any()) continue;
+                if (line.Contains("		\"id\":") && !line.Contains("			"))
+                {
+                    id = Convert.ToInt32(line.Replace("		\"id\":", "").Replace(',', ' '));
+                    achivements.Add(id, name);
+                    continue;
+                }
+                name = "";     
+            }
+            reader.Close();
+
+
+            XDocument doc = XDocument.Load(LOCAL_PATH);
+            foreach (XElement node in doc.Root.Element("name").Elements())
+            {
+                id = Convert.ToInt32(node.Name.ToString().Replace("id", ""));
+                name = node.Value.ToString();
+                if (achivements.ContainsKey(id))
+                    achivements[id] = name;
+            }
+        }
+
+        public static int getIDByName(string name)
+        {
+            foreach (KeyValuePair<int, string> pair in achivements)
+            {
+                if (pair.Value == name) return pair.Key;
+            }
+            return 0;
+        }
+
+        public static string getNameByID(int frac_id)
+        {
+            foreach (KeyValuePair<int, string> pair in achivements)
+            {
+                if (pair.Key == frac_id) return pair.Value;
+            }
+            return "";
+        }
+
+        public static string[] getListNames()
+        {
+            return achivements.Values.ToArray();
+        }
+    }
+}
---
+++ StalkerOnlineQuesterEditor/TextUtils.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace StalkerOnlineQuesterEditor
+{
+    public class WordLocation
+    {
+        public int index;
+        public int len;
+
+        public WordLocation(int index, int len)
+        {
+            this.index = index; this.len = len;
+        }
+    }
+
+    public static class TextUtils
+    {
+
+        static TextBox textbox = new TextBox();
+        static bool inited = false;
+
+        static void init()
+        {
+            inited = true;
+            textbox.Language = System.Windows.Markup.XmlLanguage.GetLanguage("ru-RU");
+            textbox.SpellCheck.IsEnabled = true;
+        }
+            
+
+        public static void findTextErrors(System.Windows.Forms.RichTextBox rtb)
+        {
+            if (!CSettings.hasErrorFinder())
+            {
+                int tmp = rtb.SelectionStart;
+                rtb.Select(0, rtb.Text.Length);
+                rtb.SelectionColor = Color.Black;
+                rtb.Select(tmp, 0);
+                
+                return;
+            }
+            if (!inited) init();
+            string text = rtb.Text;
+            textbox.Text = text;
+            //textbox.SpellCheck.CustomDictionaries.Add(new Uri(@"ru-RU.dic", UriKind.Relative));
+            int index = 0;
+            List<WordLocation> result = new List<WordLocation>();
+            while (true)
+            {
+                //находим ошибку            
+                index = textbox.GetNextSpellingErrorCharacterIndex(index, System.Windows.Documents.LogicalDirection.Forward);
+                if (index > text.Length || index < 0) break;
+
+                var error = textbox.GetSpellingError(index);
+                int len = textbox.GetSpellingErrorLength(index);
+
+                result.Add(new WordLocation(index, len));
+                /*
+                string word = textbox.Text.Substring(index, len);
+
+                sb.AppendFormat("Ошибка в слове {0}, рекомендуется заменить на одно из следующих слов: ", word);
+                */
+                //переход к следующему слову
+                index += len;
+            }
+
+            index = rtb.SelectionStart;
+
+            rtb.Select(0, text.Length);
+            rtb.SelectionColor = Color.Black;
+
+            foreach (var i in result)
+            {
+                rtb.Select(i.index, i.len);
+                rtb.SelectionColor = Color.DarkRed;
+            }
+
+
+            rtb.Select(index, 0);
+            rtb.SelectionColor = Color.Black;
+        }
+
+
+
+    }
+}
